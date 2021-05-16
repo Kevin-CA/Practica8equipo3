086dcf0fc9d13881d5df3f1edd146ba68e4d155a
Kevin-CA <78865593+Kevin-CA@users.noreply.github.com>
2021-05-15 20:45:59 -0500

    Practica 7

--- Practica2Java/src/Clases/ito/poo/CuentaBancaria.java
+++ Practica2Java/src/Clases/ito/poo/CuentaBancaria.java
@@ -13,149 +13,113 @@ import java.time.LocalDate;
  * 
  * @author Kevin
  */
-public class CuentaBancaria {
+public class CuentaBancaria implements Comparable<CuentaBancaria> {
+	
+	private long numCuenta = 0L;
+	private String nomCliente = "";
+	private float Saldo = 0F;
+	private LocalDate fechaApertura = null;
+	private LocalDate fechaActualizacion = null;
+	
+	public CuentaBancaria() {
+		
+		super();
+		
+	}
 	public CuentaBancaria(long numCuenta, String nomCliente, float saldo, LocalDate fechaApertura) {
 		super();
 		this.numCuenta = numCuenta;
 		this.nomCliente = nomCliente;
-		this.saldo = saldo;
+		Saldo = saldo;
 		this.fechaApertura = fechaApertura;
 	}
+	
+	public boolean Deposito(float Cantidad) {
+		boolean Deposito = false;
+		if(this.fechaApertura==null)
+			System.out.println("La cuenta no está activa");
+		else {
+			Deposito = true;
+			this.setSaldo(this.getSaldo()+Cantidad);
+			this.fechaActualizacion=LocalDate.now();
+		}
+		
+		return Deposito;
+	}
+	
 
-	/**
-	 * Description of the property numCuenta.
-	 */
-	public long numCuenta = 0L;
-
-	/**
-	 * Description of the property nomCliente.
-	 */
-	public String nomCliente = "";
-
-	/**
-	 * Description of the property saldo.
-	 */
-	public float saldo = 0F;
-
-	/**
-	 * Description of the property fechaApertura.
-	 */
-	public LocalDate fechaApertura = null;
-
-	/**
-	 * Description of the property fechaActualizacion.
-	 */
-	public LocalDate fechaActualizacion = null;
-
-	// Start of user code (user defined attributes for CuentaBancaria)
-
-	// End of user code
-
-	/**
-	 * The constructor.
-	 */
-	public CuentaBancaria() {
-		// Start of user code constructor for CuentaBancaria)
-		super();
-		// End of user code
+	public boolean Retiro(float Cantidad) {
+		
+		boolean Retiro = false;
+		if(Cantidad<=this.getSaldo()) {
+			Retiro = true;
+			this.setSaldo(this.getSaldo()-Cantidad);
+			this.fechaActualizacion=LocalDate.now();
+		}
+		else
+			System.out.println("La cantidad a retirar sobrepasa el saldo");
+		return Retiro;
 	}
 
-	/**
-	 * Description of the method deposito.
-	 * @param cantidad 
-	 */
-	public void deposito(float cantidad) {
-		// Start of user code for method deposito
-		// End of user code
-	}
-
-	// Start of user code (user defined methods for CuentaBancaria)
-
-	// End of user code
-	/**
-	 * Returns numCuenta.
-	 * @return numCuenta 
-	 */
+	
 	public long getNumCuenta() {
+		
 		return this.numCuenta;
 	}
 
-	/**
-	 * Sets a value to attribute numCuenta. 
-	 * @param newNumCuenta 
-	 */
 	public void setNumCuenta(long newNumCuenta) {
+		
 		this.numCuenta = newNumCuenta;
 	}
 
-	/**
-	 * Returns nomCliente.
-	 * @return nomCliente 
-	 */
+	
 	public String getNomCliente() {
 		return this.nomCliente;
 	}
 
-	/**
-	 * Sets a value to attribute nomCliente. 
-	 * @param newNomCliente 
-	 */
+	
 	public void setNomCliente(String newNomCliente) {
 		this.nomCliente = newNomCliente;
 	}
 
-	/**
-	 * Returns saldo.
-	 * @return saldo 
-	 */
 	public float getSaldo() {
-		return this.saldo;
+		return this.Saldo;
 	}
 
-	/**
-	 * Sets a value to attribute saldo. 
-	 * @param newSaldo 
-	 */
 	public void setSaldo(float newSaldo) {
-		this.saldo = newSaldo;
+		this.Saldo = newSaldo;
 	}
 
-	/**
-	 * Returns fechaApertura.
-	 * @return fechaApertura 
-	 */
 	public LocalDate getFechaApertura() {
 		return this.fechaApertura;
 	}
 
-	/**
-	 * Sets a value to attribute fechaApertura. 
-	 * @param newFechaApertura 
-	 */
 	public void setFechaApertura(LocalDate newFechaApertura) {
 		this.fechaApertura = newFechaApertura;
 	}
 
-	/**
-	 * Returns fechaActualizacion.
-	 * @return fechaActualizacion 
-	 */
 	public LocalDate getFechaActualizacion() {
 		return this.fechaActualizacion;
 	}
 
-	/**
-	 * Sets a value to attribute fechaActualizacion. 
-	 * @param newFechaActualizacion 
-	 */
 	public void setFechaActualizacion(LocalDate newFechaActualizacion) {
 		this.fechaActualizacion = newFechaActualizacion;
 	}
 
 	@Override
 	public String toString() {
-		return "CuentaBancaria [numCuenta=" + numCuenta + ", nomCliente=" + nomCliente + ", saldo=" + saldo
+		return "CuentaBancaria [numCuenta=" + numCuenta + ", nomCliente=" + nomCliente + ", Saldo=" + Saldo
 				+ ", fechaApertura=" + fechaApertura + ", fechaActualizacion=" + fechaActualizacion + "]";
+	}
+	@Override
+	public int compareTo(CuentaBancaria o) {
+		int compare=0;
+		if (this.numCuenta<o.getNumCuenta())
+			compare=-1;
+		else if(this.numCuenta>o.getNumCuenta())
+			compare=1;
+		return compare;
+		
 	}
 
 }
--- Practica2Java/src/app/ito/poo/MyApp.java
+++ Practica2Java/src/app/ito/poo/MyApp.java
@@ -1,19 +1,12 @@
 package app.ito.poo;
 
-import java.time.LocalDate;
-import Clases.ito.poo.CuentaBancaria;
-
 
 public class MyApp {
 
-	static void run() {
-		System.out.println(new CuentaBancaria(89393L,"Lucia Mendoza",15000,
-				LocalDate.of(2021, 02, 2)));
-	}
-	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		run();
+
+		Aplicacion.menu();
+		
 	}
 
 }
